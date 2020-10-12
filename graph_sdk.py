919a86f70a3bbc79b38f14c8022217d61c436063
Richard Chang <richardchang@fb.com>
2020-10-12 19:25:46 +0800

    base

---
+++ graph_sdk.py
@@ -0,0 +1,122 @@
+# Copyright 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+# based off python3
+#
+
+import requests
+#import csv_header
+import json
+import datetime
+import urllib
+#import collections
+
+#general constants
+FIRST_ITEM = 0
+EMPTY_RESPONSE = ''
+
+
+# property constants
+
+FIELD_NAME = 'name'
+FIELD_MESSAGE = 'message'
+FIELD_ID = 'id'
+FIELD_DATA = 'data'
+FIELD_PAGING = 'paging'
+FIELD_NEXT = 'next'
+FIELD_EMAIL = 'email'
+
+
+
+#request constants
+HEADER_AUTH_KEY = 'Authorization'
+HEADER_AUTH_VAL_PREFIX = 'Bearer '
+USERS_RESOURCE_SUFFIX = 'Users'
+THREAD_RESOURCE_SUFFIX = 'conversations'
+MESSAGE_RESOURCE_SUFFIX = 'messages'
+MEMBERS_RESOURCE_SUFFIX = 'members'
+COMMUNITY_RESOURCE_SUFFIX='community'
+
+HTTP_DELIM = '/'
+PARA_DELIM = '?'
+PARA_AND = '&'
+#EMAIL_LOOKUP_SUFFIX = '?filter=userName'
+#ESCAPED_EMAIL_LOOKUP_PREFIX = ' eq \"'
+#ESCAPED_EMAIL_LOOKUP_SUFFIX = '\"'
+START_INDEX = "startIndex="
+USER_FILTER = "user="
+FIELDS_FILTER = "fields="
+
+
+#response status code constants
+RESPONSE_OK = 200
+RESPONSE_CREATED = 201
+
+
+#prompt constants
+RESULT_STATUS_DELIM = ':'
+EXPORT_RESULT_DELIM = " / "
+#CREATING_USER_PROMPT = 'Creating user '
+#DELETING_USER_PROMPT = 'Deleting user '
+#UPDATING_USER_PROMPT = 'Updating user '
+
+#error constants
+ERROR_WITHOUT_MESSAGE = "Invalid request, no error message found"
+
+
+def getHeaders(access_token):
+	headers = {HEADER_AUTH_KEY: HEADER_AUTH_VAL_PREFIX + access_token}
+	return headers
+
+def getMessagesByThreads(graphurl,access_token,threadid,userid,since,fields=None, filters=None):
+	if fields : fields = PARA_AND+FIELDS_FILTER+fields
+	url = graphurl+str(threadid)+HTTP_DELIM+MESSAGE_RESOURCE_SUFFIX+PARA_DELIM+USER_FILTER+str(userid)+fields+filters;
+	return getPagedDataWithFilter(access_token,url,[],'created_time',since);
+		
+
+def getThreads(graphurl, access_token, userid, fields=None, filters=None):
+	url = graphurl+str(userid)+HTTP_DELIM+THREAD_RESOURCE_SUFFIX+fields+filters;
+	return getPagedData(access_token,url,[])
+
+def getMembers(graphurl,access_token,fields=None,filters=None) :
+	#memberColletion = [];
+	url = graphurl+HTTP_DELIM+COMMUNITY_RESOURCE_SUFFIX+HTTP_DELIM+MEMBERS_RESOURCE_SUFFIX+fields+filters;
+	return getPagedData(access_token,url,[])
+
+
+def getPagedData(access_token, endpoint, data):
+    headers = getHeaders(access_token)
+    result = requests.get(endpoint,headers=headers)
+    result_json = json.loads(result.text)
+    json_keys = result_json.keys()
+    if FIELD_DATA in json_keys and len(result_json[FIELD_DATA]):
+        data.extend(result_json[FIELD_DATA])
+    if FIELD_PAGING in json_keys and FIELD_NEXT in result_json[FIELD_PAGING]:
+        next = result_json[FIELD_PAGING][FIELD_NEXT]
+        if next:
+            getPagedData(access_token, next, data)
+    return data
+
+def getPagedDataWithFilter(access_token, endpoint, data,filter_field,filter_value):
+	headers = getHeaders(access_token)
+	result = requests.get(endpoint,headers=headers)
+	result_json = json.loads(result.text)
+	json_keys = result_json.keys()
+	if FIELD_DATA in json_keys and len(result_json[FIELD_DATA]):
+		for item in result_json[FIELD_DATA] :
+			filter_date = datetime.datetime.strptime(item[filter_field], '%Y-%m-%dT%H:%M:%S+%f')
+			if filter_date>filter_value : data.extend([item]);
+			else : return data
+
+	if FIELD_PAGING in json_keys and FIELD_NEXT in result_json[FIELD_PAGING]:
+		next = result_json[FIELD_PAGING][FIELD_NEXT]
+		if next:
+			#print('go for next page')
+			getPagedDataWithFilter(access_token, next, data,filter_field,filter_value)
+	return data
+	
+
+
---
+++ loadThreads.py
@@ -0,0 +1,139 @@
+# Copyright 2020-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+# -*- coding: utf-8 -*-
+
+#general imports
+import sys
+import re
+import time
+import datetime
+import os
+import json
+from dotenv import load_dotenv
+
+
+#project imports
+import graph_sdk
+
+
+#prompts
+START_THREADBYUSER_PROMPT = 'Getting all chats by user:';
+
+#input prompts
+ACCESS_TOKEN_INPUT_PROMPT = 'Access token? '
+USER_ID_INPUT_PROMPT = 'user id?'
+
+#error messages
+ERROR_MISSING_ACCESS_TOKEN = 'Access token is required, exiting...'
+ERROR_USER_ID = 'User id is required, exiting...'
+
+
+#other constants
+FIELD_THREAD='messages'
+FIELD_THREAD_DATA = 'data'
+FIELD_THREAD_ID = 'id'
+
+FIELD_MESSAGE_ID = 'id'
+FIELD_MESSAGE_TIME = 'created_time'
+FIELD_MESSAGE_SENDER = 'from' 
+FIELD_MESSAGE_SENDER_NAME = 'name'
+FIELD_MESSAGE_SENDER_EMAIL = 'email'
+
+
+#file constants
+FILE_WRITE_PERMISSION = 'wb'
+FILE_DELIM = ','
+FILE_QUOTE = '"'
+
+
+#url constants for validating url param
+SCIM_URL = 'https://www.facebook.com/scim/v1/'
+GRAPH_URL = 'https://graph.facebook.com/'
+
+
+#initialize
+load_dotenv();
+
+access_token = os.getenv("ACCESS_TOKEN");
+#user_id = 100046091736554;
+if os.getenv("HISTORY") : history = int(os.getenv("HISTORY"))
+else : history = 180;
+SINCE = datetime.datetime.now() - datetime.timedelta(days=history);
+
+if os.getenv("LIMIT") : LIMIT = int(os.getenv("LIMIT"));
+else : LIMIT = 50;
+
+
+def exportMessagesByThread(access_token,threadid,userid) :
+	messages = graph_sdk.getMessagesByThreads(GRAPH_URL,access_token,threadid,user_id,SINCE,'id,from,created_time','&limit='+str(LIMIT));
+	return messages;
+
+
+def exportThreadsID(access_token,user_id,threadsCol,filter_by_since=True) :
+	threads=graph_sdk.getThreads(GRAPH_URL,access_token,user_id,'?fields=id,updated_time,name','&limit='+str(LIMIT));
+
+
+	for thread in threads :
+		#thread_time = datetime.datetime.strptime(thread['updated_time'], '%Y-%m-%dT%I:%M:%S+%f');
+		if thread['id'] in threadsCol :
+			threadsCol[thread['id']]['participants'].add(user_id);
+		else : 
+			thread_time = datetime.datetime.strptime(thread['updated_time'], '%Y-%m-%dT%H:%M:%S+%f');
+			if (not filter_by_since) or (thread_time>SINCE) :
+				threadsCol[thread['id']] = {'updated_time':thread['updated_time'],'participants':{user_id}};
+	return threadsCol;
+
+def exportMembersID(access_token) :
+	members = graph_sdk.getMembers(GRAPH_URL,access_token,'?fields=id,email,name','&limit='+str(LIMIT));
+	return members;
+
+
+
+if __name__ == '__main__':
+	if not access_token :
+		access_token = input(ACCESS_TOKEN_INPUT_PROMPT);
+		if not access_token:
+			print(ERROR_MISSING_ACCESS_TOKEN)
+
+	print('Extracting conversations since :' + str(SINCE));
+	print('--------- Starting at : '+str(datetime.datetime.now()) +'---------');
+
+	#extract active members
+
+	members = exportMembersID(access_token);
+	with open('members.csv', 'w') as outfile:
+		json.dump(members, outfile)
+	print('--------- '+str(len(members))+' Members extracted by : '+str(datetime.datetime.now()) +'---------');
+
+
+	#extract threads with at least one active member
+	threads={};
+	for member in members :
+		threads = exportThreadsID(access_token,member['id'],threads);
+
+	print('--------- '+str(len(threads))+' Threads extracted by : '+str(datetime.datetime.now()) +'---------');
+
+	#extract messages within each chat 
+
+	for thread in threads :
+		print(thread + ' : ' + str(threads[thread]));
+		user_id = threads[thread]['participants'].pop();threads[thread]['participants'].add(user_id);
+		messages = exportMessagesByThread(access_token,thread,user_id)
+		for msg in messages:
+			if 'email' in msg['from'] :
+				print(thread + ': '+msg['created_time']+'  from:'+msg['from']['email']);
+		print('-------------------------------------------');
+
+	print('---------  messages extracted by : '+str(datetime.datetime.now()) +'---------');
+
+		
+
+
+
+
+
+
